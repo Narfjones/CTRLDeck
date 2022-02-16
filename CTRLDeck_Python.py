d4de0cab4ac62b9e910a2c9f3a2b21103551fbf1
Narfjones <38131442+Narfjones@users.noreply.github.com>
2022-02-16 16:01:18 -0500

    Setup Serial connection

--- CTRLDeck_Python.py
+++ CTRLDeck_Python.py
@@ -1,1 +1,25 @@
+import serial
+from time import sleep
+import sys
+import time
 
+
+ser = serial.Serial(
+    port='COM15',\
+    baudrate=9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+        timeout=0)
+
+print("connected to: " + ser.portstr)
+count=1
+
+while True:
+    line =  str(ser.readline())
+
+    print(str(count) + str(': ') + str(line) )
+    count = count+1
+    line = None
+
+ser.close()
