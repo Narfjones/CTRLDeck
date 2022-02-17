8441e97cd8b77a9d449bcded2237cf2490d2989e
Narfjones <38131442+Narfjones@users.noreply.github.com>
2022-02-17 13:33:07 -0500

    Stuck on separating serial output into separate strings.

--- CTRLDeck_Python.py
+++ CTRLDeck_Python.py
@@ -2,10 +2,10 @@ import serial
 from time import sleep
 import sys
 import time
-counter=32
+import re
 
 ser = serial.Serial(
-    port='COM20',\
+    port='COM15',\
     baudrate=9600,\
     parity=serial.PARITY_NONE,\
     stopbits=serial.STOPBITS_ONE,\
@@ -15,24 +15,41 @@ ser = serial.Serial(
 print("connected to: " + ser.portstr)
 count=1
 
+slider1str: str
+slider2str: str
+
+def getIntegers(string):
+        numbers = [int(x) for x in string.split() if x.isnumeric()]
+        return numbers
 
 
 while True:
         # create string, convert serial input data to a string a store it
         line =  str(ser.readline())
+
+        sliderlst = line.split("|")
+
+        slider1str = re.findall(r'\d+', sliderlst)
+
+
+
+        print(type(slider1str))
+               
+        sleep(.01)
+
         
         # print the count, serial data string, and up the count
-        print(str(count) + str(': ') + str(line) )
-        count +=1
+        #print(str(count) + str(': ') + slider1 + str("|") + slider2)
+        #count +=1
         
         # sleep for .02 seconds because arduino is outputting every 10 milliseconds
-        sleep(.002)
+        #sleep(.005)
         
-        # clear input buffer to dump and gathered data during our downtime
+        # clear input buffer to dump gathered data during our downtime
         ser.reset_input_buffer() 
         
         # sleep for another .02 seconds or clearing input buffer prevents new input
-        sleep(.002)
+        sleep(.01)
         
         
 ser.close()
