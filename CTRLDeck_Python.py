449d171a8a369f254b06508df9d169b01fc01ac9
Narfjones <38131442+Narfjones@users.noreply.github.com>
2022-02-16 22:58:01 -0500

    refresh input buffer. added comments

--- CTRLDeck_Python.py
+++ CTRLDeck_Python.py
@@ -2,10 +2,10 @@ import serial
 from time import sleep
 import sys
 import time
-
+counter=32
 
 ser = serial.Serial(
-    port='COM15',\
+    port='COM20',\
     baudrate=9600,\
     parity=serial.PARITY_NONE,\
     stopbits=serial.STOPBITS_ONE,\
@@ -15,11 +15,24 @@ ser = serial.Serial(
 print("connected to: " + ser.portstr)
 count=1
 
+
+
 while True:
-    line =  str(ser.readline())
-
-    print(str(count) + str(': ') + str(line) )
-    count = count+1
-    line = None
-
+        # create string, convert serial input data to a string a store it
+        line =  str(ser.readline())
+        
+        # print the count, serial data string, and up the count
+        print(str(count) + str(': ') + str(line) )
+        count +=1
+        
+        # sleep for .02 seconds because arduino is outputting every 10 milliseconds
+        sleep(.002)
+        
+        # clear input buffer to dump and gathered data during our downtime
+        ser.reset_input_buffer() 
+        
+        # sleep for another .02 seconds or clearing input buffer prevents new input
+        sleep(.002)
+        
+        
 ser.close()
