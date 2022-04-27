dc9fa3568cfe69744d74d1a5d80329220f41d8de
Narfjones <38131442+Narfjones@users.noreply.github.com>
2022-04-26 23:24:37 -0400

    minor changes

--- CTRLDeck_kivy.py
+++ CTRLDeck_kivy.py
@@ -3,18 +3,45 @@ kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 import kivy.properties as prop
 from kivy.vector import Vector
 from kivy.clock import Clock
 
 
-class Fader(Widget):
-    velocity_x = prop.NumericProperty(0)
-    velocity_y = prop.NumericProperty(0)
-    velocity = prop.ReferenceListProperty(velocity_x, velocity_y)
+class Buttons(GridLayout):
+    pass
 
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+class Boxes(GridLayout):
+    pass
+"""    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b13 = Button(text="F13")
+        b14 = Button(text="F14")
+        b15 = Button(text="F15")
+        b16 = Button(text="F16")
+        b17 = Button(text="F17")
+        b18 = Button(text="F18")
+        b19 = Button(text="F19")
+        b20 = Button(text="F20")
+        b21 = Button(text="F21")
+        b22 = Button(text="F22")
+        b23 = Button(text="F23")
+        b24 = Button(text="F24")
+        self.add_widget(b13)
+        self.add_widget(b14)
+        self.add_widget(b15)
+        self.add_widget(b16)
+        self.add_widget(b17)
+        self.add_widget(b18)
+        self.add_widget(b19)
+        self.add_widget(b20)
+        self.add_widget(b21)
+        self.add_widget(b22)
+        self.add_widget(b23)
+        self.add_widget(b24)
+"""
 
 class CTRLdeck(Widget):
     pass
@@ -22,7 +49,7 @@ class CTRLdeck(Widget):
 
 class CTRLdeckApp(App):
     def build(self):
-        return CTRLdeck()
+        return Boxes()
 
 
 if __name__ == '__main__':
