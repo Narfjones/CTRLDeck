a4850b81d3660167c45f6a59698bdfdc75530b58
Narfjones <38131442+Narfjones@users.noreply.github.com>
2022-05-03 16:22:35 -0400

    made changes

--- CTRLdeck_pyqt5.py
+++ CTRLdeck_pyqt5.py
@@ -1,12 +1,16 @@
 from random import triangular
 import sys
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (
     QApplication,
     QCheckBox,
+    QComboBox,
     QFormLayout,
+    QGridLayout,
     QLineEdit,
     QHBoxLayout,
+    QSlider,
     QVBoxLayout,
     QWidget,
     QMainWindow,
@@ -17,37 +21,73 @@ from PyQt5.QtWidgets import (
 
 from PyQt5.QtGui import QPalette, QColor
 
+sliders = [1, 2, 3, 4]
+macroKeys = []
+sessionOptions = ["master", "chrome.exe", "firefox.exe", "discord.exe", "microphone", "unmapped", "Choose a file:" ]
+
 class MainWindow(QMainWindow):
         
         def __init__ (self):
             super(MainWindow, self).__init__()
 
+            central_widget = QWidget()
+            layout = QVBoxLayout()
+            central_widget.setLayout(layout)
+            self.setCentralWidget(central_widget)
+
             self.setWindowTitle("CTRLdeck")
             self.setGeometry(500,250,1023,707)
 
-            mainWidget = leftMenu()
-            self.setCentralWidget(mainWidget)
+            topMenu = TopMenu()
+            leftMenu = LeftMenu()
+            layout.addWidget(topMenu)
+            layout.addWidget(leftMenu)
 
-def leftMenu():
+            
+
+def TopMenu():
+    topLabel = QLabel()
+    topImg = QPixmap('ctrldeck-title.png')
+    topLabel.setPixmap(topImg)
+    return topLabel
+
+def LeftMenu():
     leftMenu = QTabWidget()
     leftMenu.setTabPosition(QTabWidget.West)
     leftMenu.setMovable(False)
-    leftMenu.addTab(MainMenu(), "Main Menu")
-    leftMenu.addTab(SliderMenu(), "Sliders")
-    leftMenu.addTab(MacroKeysMenu(), "Macro Keys")
+    mainTab = QWidget()
+    sliderTab = QWidget()
+    macroTab = QWidget()
+    leftMenu.addTab(mainTab, "Main Menu")
+    leftMenu.addTab(sliderTab, "Sliders")
+    leftMenu.addTab(macroTab, "Macro Keys")
+    mainTab.setLayout(MainMenuUI())
+    sliderTab.setLayout(SliderMenuUI())
+    macroTab.setLayout(MacroKeysMenuUI())
     return leftMenu
 
     
-def MainMenu():
-    mainMenu = QWidget()
+def MainMenuUI():
+    mainMenu = QHBoxLayout()
     return mainMenu
 
-def SliderMenu():
-    sliderMenu = QWidget()
-    return sliderMenu
+def SliderMenuUI():
+    layout = QGridLayout()
+    for x in sliders:
+        process = QComboBox()
+        for i in sessionOptions:
+            process.addItem(i)
+        fader = QSlider(Qt.Vertical)
+        fader.setTickPosition(QSlider.TicksLeft)
+        fader.setMinimum(0)
+        fader.setMaximum(100)
+        fader.setTickInterval(5)
+        layout.addWidget(process, 1, x)
+        layout.addWidget(fader, 2, x)
+    return layout
 
-def MacroKeysMenu():
-    macroKeysMenu = QWidget()
+def MacroKeysMenuUI():
+    macroKeysMenu = QGridLayout()
     return macroKeysMenu
 
     
@@ -55,7 +95,7 @@ app = QApplication(sys.argv)
 
 window = MainWindow()
 
-with open("./inc/style.css", "r") as file:
+with open("style.css", "r") as file:
     app.setStyleSheet(file.read())
 
 window.show()
